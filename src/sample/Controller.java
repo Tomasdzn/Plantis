4960a27e177ed87068dffebb705215f8cca18be5
Tomas <34645484+Tomasdzn@users.noreply.github.com>
2019-06-21 15:33:55 +0200

    Login tab

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,10 +6,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
@@ -23,6 +20,8 @@ public class Controller {
     // Variables misc.
     private double xOffset = 0;
     private double yOffset = 0;
+    @FXML
+    private MenuItem meLogin;
 
     // Variables de Welcome
     @FXML
@@ -42,7 +41,13 @@ public class Controller {
     @FXML
     private PasswordField txtSecret;
     @FXML
-    private Label lblWarning;
+    private Label calbum_lblWarning;
+
+    // Variables del Login
+    @FXML
+    private Label login_lblWarning;
+    @FXML
+    private Button login_btnCancel;
 
     @FXML
     private void close(ActionEvent event){
@@ -51,8 +56,14 @@ public class Controller {
     }
 
     @FXML
-    private void closeNewAlb(ActionEvent event){
+    private void closeNewAlb(){
         Stage st = (Stage) btnCancel.getScene().getWindow();
+        st.close();
+    }
+
+    @FXML
+    private void closeLogin(){
+        Stage st = (Stage) login_btnCancel.getScene().getWindow();
         st.close();
     }
 
@@ -112,5 +123,35 @@ public class Controller {
         } else {
             txtSecret.setDisable(true);
         }
+    }
+
+    @FXML
+    private void loginTab() throws IOException {
+        Stage stage = new Stage();
+        stage.getIcons().add(new Image(".//resources//leaf.png"));
+        stage.setTitle("Login");
+        stage.setResizable(false);
+
+        stage.initStyle(StageStyle.UNDECORATED);
+        Parent root = FXMLLoader.load(getClass().getResource("loginAccount.fxml"));
+
+        FadeTransition ft = new FadeTransition(Duration.millis(500), root);
+        ft.setFromValue(0.0);
+        ft.setToValue(1.0);
+        ft.play();
+
+        root.setOnMousePressed(event1 -> {
+            xOffset = event1.getSceneX();
+            yOffset = event1.getSceneY();
+        });
+        root.setOnMouseDragged(event12 -> {
+            stage.setX(event12.getScreenX() - xOffset);
+            stage.setY(event12.getScreenY() - yOffset);
+        });
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
     }
 }
